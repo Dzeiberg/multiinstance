232c4f5a62477112b80624a7cbf6c48ad82f76f5
Daniel Zeiberg <zeiberg.d@northeastern.edu>
2021-01-29 10:30:04 -0500

    push notebook updates

--- multiinstance/_nbdev.py
+++ multiinstance/_nbdev.py
@@ -2,9 +2,32 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"getBag": "02_SyntheticData.ipynb",
+         "buildDatasetDict": "02_SyntheticData.ipynb",
+         "Dataset": "02_SyntheticData.ipynb",
+         "buildDataset": "02_SyntheticData.ipynb",
+         "estimator": "03_Dataset_Utils.ipynb",
+         "getBootstrapSample": "03_Dataset_Utils.ipynb",
+         "getEsts": "03_Dataset_Utils.ipynb",
+         "getBagAlphaHats": "03_Dataset_Utils.ipynb",
+         "getCliqueAlphaHats": "03_Dataset_Utils.ipynb",
+         "getAlphaPrime": "03_Dataset_Utils.ipynb",
+         "getKSMatrixPMatrix": "03_Dataset_Utils.ipynb",
+         "getAllCliques": "03_Dataset_Utils.ipynb",
+         "clusterByLeidenAlg": "03_Dataset_Utils.ipynb",
+         "getOptimalAdjacency": "03_Dataset_Utils.ipynb",
+         "addTransformScores": "04_Distribution_Distance_Approaches.ipynb",
+         "splitIntoBags": "04_Distribution_Distance_Approaches.ipynb",
+         "fitKDE": "04_Distribution_Distance_Approaches.ipynb",
+         "KLD": "04_Distribution_Distance_Approaches.ipynb",
+         "JSD": "04_Distribution_Distance_Approaches.ipynb",
+         "getJSDDistMat": "04_Distribution_Distance_Approaches.ipynb",
+         "getKLDMat": "04_Distribution_Distance_Approaches.ipynb",
+         "getWassersteinMat": "04_Distribution_Distance_Approaches.ipynb"}
 
-modules = []
+modules = ["data/syntheticData.py",
+           "utils.py",
+           "distanceApproaches.py"]
 
 doc_url = "https://Dzeiberg.github.io/multiinstance/"
 
