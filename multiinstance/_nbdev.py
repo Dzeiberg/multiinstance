76a8683cdc145054e4f8b6a1f4525a5b89f04434
Daniel Zeiberg <zeiberg.d@northeastern.edu>
2021-03-25 13:48:39 -0400

    update nbs

--- multiinstance/_nbdev.py
+++ multiinstance/_nbdev.py
@@ -54,7 +54,8 @@ index = {"getBag": "02_SyntheticData.ipynb",
          "plotMAE": "22_Quarantine_DistCurve.ipynb",
          "plotDistrs": "22_Quarantine_DistCurve.ipynb",
          "prepDS": "24_Likelihood_Cleaned.ipynb",
-         "LikelihoodMethod": "25_Likelihood_With_Ward.ipynb"}
+         "LikelihoodMethod": "25_Likelihood_With_Ward.ipynb",
+         "getSequence": "27_Read_Varibench.ipynb"}
 
 modules = ["data/syntheticData.py",
            "utils.py",
@@ -65,7 +66,8 @@ modules = ["data/syntheticData.py",
            "data/realData.py",
            "likelihoodMethods.py",
            "likelihood_method.py",
-           "tree_likelihood.py"]
+           "tree_likelihood.py",
+           "data/gene.py"]
 
 doc_url = "https://Dzeiberg.github.io/multiinstance/"
 
